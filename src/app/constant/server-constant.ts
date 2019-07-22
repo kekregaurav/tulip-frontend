c88477752e4cb3576a043c36dead6261a67e08a9
Kekre <kekre@uspsector.com>
2019-07-22 14:53:18 -0500

    second commit

---
+++ src/app/constant/server-constant.ts
@@ -0,0 +1,10 @@
+export class ServerConstant {
+
+    public host = 'http://localhost:8080/tulip';
+
+    public client = 'http://localhost:4200';
+
+    public userPicture = '${this.host}/image/user';
+
+    public postPicture = '${this.host}/image/post';
+}
---
+++ src/app/enum/alert-type.enum.ts
@@ -0,0 +1,5 @@
+export enum AlertType {
+    SUCCESS = 'success',
+    DANGER = 'danger'
+  }
+  
